b530037de0aab31b8f6e9f39279b45ce0e04424b
Morten Paulsen <mortepau@stud.ntnu.no>
2019-01-21 20:31:41 +0100

    Finished C and Python parts

--- Exercise2/Part2/python/foo.py
+++ Exercise2/Part2/python/foo.py
@@ -2,34 +2,41 @@
 # python fo.py
 
 from threading import Thread
+from threading import Lock
 
 # Potentially useful thing:
 #   In Python you "import" a global variable, instead of "export"ing it when you declare it
 #   (This is probably an effort to make you feel bad about typing the word "global")
 i = 0
+lock = Lock()
 
 def incrementingFunction():
-    global i
+	global i, lock
     # TODO: increment i 1_000_000 times
+	for c in range(1000000):
+		with lock:
+			i += 1
 
 def decrementingFunction():
-    global i
+	global i, lock
     # TODO: decrement i 1_000_000 times
-
-
+	for c in range(1000000):
+		with lock:
+			i -= 1
 
 def main():
-    global i
+	global i, lock
+	
+	incrementing = Thread(target = incrementingFunction, args = (),)
+	decrementing = Thread(target = decrementingFunction, args = (),)
+	
+	# TODO: Start both threads
+	incrementing.start()
+	decrementing.start()
 
-    incrementing = Thread(target = incrementingFunction, args = (),)
-    decrementing = Thread(target = decrementingFunction, args = (),)
-    
-    # TODO: Start both threads
-    
-    incrementing.join()
-    decrementing.join()
-    
-    print("The magic number is %d" % (i))
+	incrementing.join()
+	decrementing.join()
 
+	print("The magic number is %d" % (i))
 
 main()
